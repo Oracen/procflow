eee3dc506e6f585cace1c1596be52529e5236298
Oracen <alex.stenlake@gmail.com>
2023-01-02 17:21:23 +1000

    Graph can build with self loops

--- topo/graph_integration_test.go
+++ topo/graph_integration_test.go
@@ -4,13 +4,38 @@ import (
 	"testing"
 
 	"github.com/Oracen/bpmn-flow/topo"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGraphBuildFunctionality(t *testing.T) {
+
+	getVertices := func(count int) (testVertices []topo.Vertex) {
+
+		testVertices = []topo.Vertex{}
+		for idx := 0; idx < count; idx++ {
+			testVertices = append(testVertices, topo.Vertex{uuid.New().String(), topo.VertexData{uuid.New().String()}})
+		}
+		return
+	}
+
 	t.Run(
-		"test external package can utilise data",
+		"test external package can utilise data and build self loops",
 		func(t *testing.T) {
 			graph := topo.NewGraph()
+			for _, item := range getVertices(4) {
+				name := uuid.New().String()
+				err := graph.AddNewVertex(name, item)
+				assert.Nil(t, err)
+				for key, _ := range graph.GetAllVertices(true) {
+					edge := topo.Edge{name, key, topo.EdgeData{InvocationName: "blah"}}
+					err = graph.AddNewEdge(uuid.New().String(), edge)
+					assert.Nil(t, err)
+				}
+			}
+			assert.Len(t, graph.GetAllVertices(false), 4)
+			assert.Len(t, graph.GetAllEdges(false), 10)
+
 		},
 	)
 }
