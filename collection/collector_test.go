04ca029ea73e4ca92c2c8552289a589780a4b06a
Oracen <alex.stenlake@gmail.com>
2023-01-03 16:21:19 +1000

    Add test to ensure async code works

--- collection/collector_test.go
+++ collection/collector_test.go
@@ -11,16 +11,21 @@ func TestCollectorCanInitialise(t *testing.T) {
 	t.Run(
 		"test collector can add and merge nontrivial objects that implement Collectable",
 		func(t *testing.T) {
+			nExtra := 5000
 			collectable := mockCollectable{[]int{0}}
 			collection := CreateNewCollector[int, mockCollectable](&collectable)
-			for idx := 1; idx < 4; idx++ {
-				err := collection.AddRelationship(idx)
-				assert.Nil(t, err)
+			for idx := 0; idx < nExtra; idx++ {
+				value := idx + 1
+				go func() {
+					err := collection.AddRelationship(value)
+					assert.Nil(t, err)
+				}()
+
 			}
 
 			merged, err := collection.UnionRelationships(mockCollectable{[]int{9, 8}})
 			assert.Nil(t, err)
-			assert.Len(t, merged.collection, 6)
+			assert.Len(t, merged.collection, nExtra+3)
 		},
 	)
 
