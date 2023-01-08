bb6ffbbc042b7e1e34cdd0eef08d7e5054b7b4df
Oracen <alex.stenlake@gmail.com>
2023-01-08 17:06:14 +1000

    Move annotation functions to their own module

--- flow_integration_test.go
+++ flow_integration_test.go
@@ -1,50 +1,92 @@
 package flow_test
 
 import (
+	"context"
 	"testing"
 
-	"github.com/Oracen/procflow/basic"
+	"github.com/Oracen/procflow/annotation/graph"
 )
 
-func proc1func1() (int, error) {
+func proc1func1(ctx context.Context) (int, error) {
+	// Fake some functions in here, just add the nodes
+	tracker := graph.RegisterTracker(ctx)
+	defer tracker.CloseTrace()
+	ctx, nodeStart := graph.Start(ctx, &tracker, "inner1Input", "First inner input node")
+
+	_, node1 := graph.Task(ctx, &tracker, []graph.Node{nodeStart}, "inner1Intermediate", "Task in first process")
+
+	graph.End(&tracker, []graph.Node{node1}, "inner1Finish", "Finish first process", false)
 	return 1, nil
 }
 
-func proc1func2(int) (string, error) {
+func proc1func2(ctx context.Context, input int) (string, error) {
+	// Fake more nodes
+	tracker := graph.RegisterTracker(ctx)
+	defer tracker.CloseTrace()
+	ctx, nodeStart := graph.Start(ctx, &tracker, "inner2Input", "Our input node")
+
+	ctx, node1 := graph.Task(ctx, &tracker, []graph.Node{nodeStart}, "inner2Intermediate", "Second process, first task")
+
+	_, node2 := graph.Task(ctx, &tracker, []graph.Node{node1}, "inner2Intermediate2", "Second process, second task")
+
+	graph.End(&tracker, []graph.Node{node2}, "inner2Finish", "Finish second process", false)
 	return "string", nil
 }
 
-func proc1func3(string) error {
+func proc1func3(ctx context.Context, inputInt int, inputStr string) error {
+	// More dummy input
+	tracker := graph.RegisterTracker(ctx)
+	defer tracker.CloseTrace()
+	ctx, nodeStart := graph.Start(ctx, &tracker, "inner3Input", "Third inner input node")
+
+	_, node1 := graph.Task(ctx, &tracker, []graph.Node{nodeStart}, "inner3Intermediate", "Task in third process")
+
+	graph.End(&tracker, []graph.Node{node1}, "inner3Finish", "Finish third process", false)
 	return nil
 }
 
 // Current mode is ugly and manual, but we need to start somewhere
-func proc1() {
+func proc1(ctx context.Context, willFailOn int) {
 	// No error branches should fail
-	tracker := basic.RegisterTracker()
-	nodeStart := tracker.StartFlow("input")
+	tracker := graph.RegisterTracker(ctx)
+	defer tracker.CloseTrace()
 
-	node1 := tracker.AddNode([]basic.Node{nodeStart}, "intermediate")
-	out1, err := proc1func1()
+	ctx, nodeStart := graph.Start(ctx, &tracker, "input", "Our input node")
 
-	if err != nil {
-		tracker.EndFlow([]basic.Node{node1}, "error")
+	ctx, node1 := graph.Task(ctx, &tracker, []graph.Node{nodeStart}, "intermediate", "Top-level task")
+	out1, err := proc1func1(ctx)
+
+	if err != nil || willFailOn == 0 {
+		graph.End(&tracker, []graph.Node{node1}, "error", "first error node", true)
 		return
 	}
-	node2 := tracker.AddNode([]basic.Node{node1}, "intermediate")
-	out2, err2 := proc1func2(out1)
-	if err2 != nil {
-		tracker.EndFlow([]basic.Node{node1}, "error")
+
+	ctx, node2 := graph.Task(ctx, &tracker, []graph.Node{node1}, "intermediate2", "Top-level task with int input")
+	out2, err2 := proc1func2(ctx, out1)
+	if err2 != nil || willFailOn == 1 {
+		graph.End(&tracker, []graph.Node{node2}, "error2", "second error node", true)
+		return
 	}
 
-	err3 := proc1func3(out2)
-	if err3 != nil {
-		tracker.EndFlow([]basic.Node{node2}, "error")
+	ctx, node3 := graph.Task(ctx, &tracker, []graph.Node{node1, node2}, "intermediate3", "Top-level task with mixed input")
+	err3 := proc1func3(ctx, out1, out2)
+	if err3 != nil || willFailOn == 2 {
+		graph.End(&tracker, []graph.Node{node3}, "error3", "third error node", true)
+		return
 	}
-	tracker.EndFlow([]basic.Node{node2}, "finish")
+	graph.End(&tracker, []graph.Node{node3}, "finish", "Final top level node - success", false)
 
 }
 
-func TestPublicApi(t *testing.T) {
+func TestDemoFlow(t *testing.T) {
+	t.Run(
+		"test node name conversion functions",
+		func(t *testing.T) {
+			ctx := context.Background()
+			for idx := 0; idx < 3; idx++ {
+				proc1(ctx, idx)
+			}
 
+		},
+	)
 }
